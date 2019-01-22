c15837231290fcc1117bf259968db4825ce72bd8
Uriel Kugelmass <urikugelmass@gmail.com>
2019-01-21 22:37:16 -0600

    Add build

---
+++ frontend/public/build/precache-manifest.8513e7206fb7ab13deb4053f0913bca8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "64f3eda8bca523fa62a7c012def27224",
+    "url": "/static/media/Carousel-4.64f3eda8.png"
+  },
+  {
+    "revision": "5d5912a0f61349d18b0d",
+    "url": "/static/css/main.fb33cabc.chunk.css"
+  },
+  {
+    "revision": "d1afc6054f47e8906fadce8c4e00ba75",
+    "url": "/static/media/renner.d1afc605.png"
+  },
+  {
+    "revision": "002ccb889e4ceb7b8543",
+    "url": "/static/js/1.002ccb88.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3fe593978f29ebf0d216cd322b5e359",
+    "url": "/static/media/Carousel-1.e3fe5939.png"
+  },
+  {
+    "revision": "bf52a90f9030ecfc3c05daf608ab4786",
+    "url": "/static/media/Carousel-2.bf52a90f.png"
+  },
+  {
+    "revision": "c79d5a9ecc2114a539c21306f6aa76eb",
+    "url": "/static/media/Carousel-3.c79d5a9e.png"
+  },
+  {
+    "revision": "5d5912a0f61349d18b0d",
+    "url": "/static/js/main.5d5912a0.chunk.js"
+  },
+  {
+    "revision": "2ab495c06203e4706917990b46b0b2d8",
+    "url": "/static/media/Carousel-5.2ab495c0.png"
+  },
+  {
+    "revision": "3bab6cbc4c02d22bb64150c706e5e505",
+    "url": "/static/media/MK-logo.3bab6cbc.jpg"
+  },
+  {
+    "revision": "8c22cffe967d3dab93c2aac43d4dc74e",
+    "url": "/static/media/estacionamiento.8c22cffe.png"
+  },
+  {
+    "revision": "87e41802438d7ebb4a2cc6ee1c53bff3",
+    "url": "/static/media/aloy.87e41802.png"
+  },
+  {
+    "revision": "a0ca16432e3129f14fa4262b1c38900f",
+    "url": "/static/media/disco.a0ca1643.png"
+  },
+  {
+    "revision": "c08da809dfad08cbdaa3bba0a0621cc7",
+    "url": "/static/media/aloft.c08da809.png"
+  },
+  {
+    "revision": "002ccb889e4ceb7b8543",
+    "url": "/static/css/1.ec75ed7d.chunk.css"
+  },
+  {
+    "revision": "81116e0dc76e73b40364ceabcc9245e5",
+    "url": "/index.html"
+  }
+];
